faab186cec709dd7b738043d0c02ddcbde5a2408
unknown <novianasr@students.amikom.ac.id>
2025-08-11 14:36:38 +0700

    view_login

---
+++ application/views/auth/login.php
@@ -0,0 +1,67 @@
+<div class="container">
+
+    <!-- Outer Row -->
+    <div class="row justify-content-center">
+
+        <div class="col-lg-7">
+
+            <div class="card o-hidden border-0 shadow-lg my-5">
+                <div class="card-body p-0">
+                    <!-- Nested Row within Card Body -->
+                    <div class="row">
+                        <div class="col-lg">
+                            <div class="p-5">
+                                <div class="text-center">
+                                    <img src="https://pertanian.uma.ac.id/wp-content/uploads/2019/12/UGM.png"
+                                        alt="Logo UGM"
+                                        style="height: 40px; margin-right: 10px;">
+                                    <div>
+                                        <h1 class="h4 text-gray-900">Single Sign On</h1>
+                                        <p> Plis login with email ugm</p>
+                                    </div>
+                                    <form class="user">
+                                        <div class="form-group">
+                                            <input type="text" class="form-control form-control-user"
+                                                id="email"
+                                                name="email"
+                                                placeholder="Enter Email Address...">
+                                        </div>
+                                        <div class="form-group">
+                                            <input type="password" class="form-control form-control-user"
+                                                id="password"
+                                                name="password"
+                                                placeholder="Password">
+                                        </div>
+                                        <div div class="form-group" style="text-align: left;">
+                                            <div class="custom-control custom-checkbox small">
+                                                <input type="checkbox" class="custom-control-input" id="customCheck">
+                                                <label class="custom-control-label" for="customCheck">Remember
+                                                    Me</label>
+                                            </div>
+                                        </div>
+                                        <button type="submit" class="btn btn-success btn-user btn-block">
+                                            Login
+                                        </button>
+                                        <hr>
+                                        <button href="index.html" class="btn btn-warning btn-user btn-block">
+                                            <i class="fab fa-google fa-fw"></i> Login with Google
+                                        </button>
+                                    </form>
+                                    <hr>
+                                    <div class="text-center">
+                                        <a class="small" href="forgot-password.html">Forgot Password?</a>
+                                    </div>
+                                    <div class="text-center">
+                                        <a class="small" href="register.html">Create an Account!</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+
+        </div>
+
+    </div>
