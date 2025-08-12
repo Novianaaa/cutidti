745ac7497c0554633e5e87172f33fa6ab066a152
novianasr@students.amikom.ac.id <novianasr@students.amikom.ac.id>
2025-08-12 16:02:58 +0700

    validation_form_registratiion

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -3,18 +3,43 @@ defined('BASEPATH') or exit('No direct script access allowed');
 
 class Auth  extends CI_Controller
 {
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->library('form_validation');
+    }
+
     public function index()
     {
         $this->load->view('templates/auth_header');
         $this->load->view('auth/login');
         $this->load->view('templates/auth_footer');
     }
-    
+
     public function registration()
     {
-        $this->load->view('templates/auth_header');
-        $this->load->view('auth/registration');
-        $this->load->view('templates/auth_footer');
-        
+
+        $this->form_validation->set_rules('name', 'Name', 'required|trim');
+        $this->form_validation->set_rules('email', 'Email', 'required|trim|valid_email');
+        $this->form_validation->set_rules(
+            'password1',
+            'Password',
+            'required|trim|min_lenght[3]|matches[password2]',
+            [
+                'matches' => 'Password dont match!',
+                'min_lenght' => 'Password too short!'
+            ]
+        );
+        $this->form_validation->set_rules('password2', 'Password', 'required|trim|matches[password1]');
+
+        if ($this->form_validation->run() == false) {
+
+            $data['title'] = 'DTI Registration';
+            $this->load->view('templates/auth_header', $data);
+            $this->load->view('auth/registration');
+            $this->load->view('templates/auth_footer');
+        } else {
+            echo 'data berhasil ditambahkan!';
+        }
     }
 }
--- application/views/templates/auth_header.php
+++ application/views/templates/auth_header.php
@@ -9,7 +9,7 @@
     <meta name="description" content="">
     <meta name="author" content="">
 
-    <title>Login Cuti DTI</title>
+    <title><?= $title; ?></title>
     <?php  ?>
     <!-- Custom fonts for this template-->
     <link href="<?= base_url('assets/'); ?>vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
